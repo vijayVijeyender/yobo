4bd0d37d4c49178f65885118937e7178e6e12851
vijayvijeyender <vijayvijeyender@gmail.com>
2022-10-04 11:49:18 +0530

    Empty Springboot Project

---
+++ src/main/java/com/vijay/yobo/YoboApplication.java
@@ -0,0 +1,13 @@
+package com.vijay.yobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoboApplication.class, args);
+	}
+
+}
