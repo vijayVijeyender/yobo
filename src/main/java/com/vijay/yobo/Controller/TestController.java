e79c2270ff9ab08f5fdfba8fda45bcf77da3a7a1
vijayvijeyender <vijayvijeyender@gmail.com>
2022-10-04 12:00:17 +0530

    Test Controller changes

---
+++ src/main/java/com/vijay/yobo/Controller/TestController.java
@@ -0,0 +1,16 @@
+package com.vijay.yobo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String testFunction(){
+        return "test successful";
+    }
+    
+}
