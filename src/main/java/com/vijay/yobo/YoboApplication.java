ae35c7e29849c666b347568447126dec1d7b4a22
vijayvijeyender <vijayvijeyender@gmail.com>
2022-10-07 09:35:44 +0530

    tomcat changes

--- src/main/java/com/vijay/yobo/YoboApplication.java
+++ src/main/java/com/vijay/yobo/YoboApplication.java
@@ -2,9 +2,10 @@ package com.vijay.yobo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class YoboApplication {
+public class YoboApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(YoboApplication.class, args);
